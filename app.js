8fe3cceb522b1c8687c7db8f8bcd56410078c4b5
vatsalintech <156601691+vatsalintech@users.noreply.github.com>
2024-02-27 22:44:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "~!@#$%^&*()_+/";
+
+const passBox = document.getElementById("pass-box");
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
+
+function charLimit (totalChar) {
+    if(totalChar > 30){
+        return 30;
+    }
+    if(totalChar < 2){
+        return 2;
+    }
+    return totalChar;
+}
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)];
+}
+
+const generatePassword = (password = "") => {
+    if(upperInput.checked){
+        password+=getRandomData(upperSet);
+    }
+    if(lowerInput.checked){
+        password+=getRandomData(lowerSet);
+    }
+    if(numberInput.checked){
+        password+=getRandomData(numberSet);
+    }
+    if(symbolInput.checked){
+        password+=getRandomData(symbolSet);
+    }
+    if(password.length < totalChar.value){
+        return generatePassword(password);
+    }
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+    totalChar.value = charLimit(totalChar.value);
+    totalChar.innerText = totalChar.value;
+    generatePassword();
+})
+
+function truncateString(str, num){
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    }
+    else{
+        return str;
+    }
+}
